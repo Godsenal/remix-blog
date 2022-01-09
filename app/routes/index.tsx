b24204d525e159bc79e6167e5f555dd16366778d
godsenal <tmqps78@gmail.com>
2022-01-09 16:29:45 +0900

    :sparkles: 홈 추가

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,38 +1,13 @@
+import { Link } from "remix";
+import PageLayout from "~/components/PageLayout";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-            className="text-3xl"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="block border-l pl-4 -ml-px border-transparent hover:border-gray-400 dark:hover:border-gray-500 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <PageLayout>
+      <h1 className="text-3xl mb-10">오호</h1>
+      <section className="">
+        <Link to="/posts">Post 보러가기</Link>
+      </section>
+    </PageLayout>
   );
 }
