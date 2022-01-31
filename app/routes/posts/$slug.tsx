f68ad007959e3927aca475bd19d3d2ce20367788
godsenal <tmqps78@gmail.com>
2022-02-01 04:56:15 +0900

    :sparkles: 글 삭제처리 추가

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,12 +1,49 @@
-import { LinksFunction, LoaderFunction, useLoaderData } from "remix";
+import {
+  Form,
+  LinksFunction,
+  LoaderFunction,
+  redirect,
+  useLoaderData,
+  useOutletContext,
+  useParams,
+} from "remix";
 import PageLayout from "~/components/PageLayout";
 import db from "~/db/db.server";
-import { Post } from "@prisma/client";
-import { Heading } from "@chakra-ui/react";
+import { Post, User } from "@prisma/client";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import Editor from "~/components/Editor";
 import { highlightCSS } from "~/utils/editor";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
+import ChakraLink from "~/components/ChakraLink";
+import { TOutletContext } from "~/types/context";
+import { getUserId } from "~/utils/auth.server";
 
 export const links: LinksFunction = () => [highlightCSS];
+
+export const action: LoaderFunction = async ({ request, params }) => {
+  const userId = await getUserId(request);
+
+  const result = await db.post.findUnique({
+    where: { post_id: Number(params.slug) },
+  });
+
+  if (result?.author_id !== Number(userId)) {
+    throw new Response("Forbidden", { status: 403 });
+  }
+
+  await db.post.delete({ where: { post_id: Number(params.slug) } });
+
+  return redirect("/");
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
   try {
@@ -16,7 +53,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 
     const post = await db.post.findFirst({
       where: { post_id: Number(params.slug) },
-      orderBy: {},
+      include: { author: true },
     });
 
     return post;
@@ -28,12 +65,37 @@ export const loader: LoaderFunction = async ({ params }) => {
 };
 
 const PostView = () => {
-  const { title, content } = useLoaderData<Post>();
+  const { user } = useOutletContext<TOutletContext>();
+  const { slug } = useParams();
+  const { title, content, author, created_at } = useLoaderData<
+    Post & { author: User }
+  >();
+  const isMine = user?.user_id === author?.user_id;
 
   return (
     <PageLayout>
-      <Heading>{title}</Heading>
-      {content && <Editor content={content} isViewer />}
+      <Stack>
+        <Heading>{title}</Heading>
+        <HStack>
+          <HStack>
+            <Text>{author.name || "temp"}</Text>
+            <Text>·</Text>
+            <Text>{format(parseISO(created_at.toString()), "yyyy-MM-dd")}</Text>
+          </HStack>
+          <Spacer />
+          {isMine && (
+            <>
+              <ChakraLink to={`/posts/${slug}/edit`}>수정</ChakraLink>
+              <Form method="post">
+                <Button type="submit" variant="unstyled" color="red.400">
+                  삭제
+                </Button>
+              </Form>
+            </>
+          )}
+        </HStack>
+        {content && <Editor content={content} isViewer />}
+      </Stack>
     </PageLayout>
   );
 };
